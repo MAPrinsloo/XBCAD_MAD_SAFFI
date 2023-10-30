04c08a1646e9f28e531ecfddafdc0baf8bfe6714
MAPrinsloo <notmattherwprinsloo@gmail.com>
2023-10-30 11:22:26 +0200

    finished UI for ExhibitionActivity.kt finished UI for both fragments implemented social and settings fragment for home

--- app/src/main/java/mad/saffi/ExhibitionActivity.kt
+++ app/src/main/java/mad/saffi/ExhibitionActivity.kt
@@ -2,10 +2,25 @@ package mad.saffi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.Slide
+import android.transition.TransitionManager
+import android.view.Gravity
+import mad.saffi.databinding.ActivityExhibitionBinding
 
 class ExhibitionActivity : AppCompatActivity() {
+    //binding
+    lateinit var ExhibitionBinding: ActivityExhibitionBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exhibition)
+
+        ExhibitionBinding = ActivityExhibitionBinding.inflate(layoutInflater)
+        val ExhibitionView = ExhibitionBinding.root
+        setContentView(ExhibitionView)
+
+        ExhibitionBinding.btnAcknowledgements.setOnClickListener()
+        {
+
+        }
     }
 }
