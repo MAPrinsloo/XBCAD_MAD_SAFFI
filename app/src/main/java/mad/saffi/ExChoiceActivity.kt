813641d275685609796faa3e649464a5a638c7ce
MAPrinsloo <notmattherwprinsloo@gmail.com>
2023-10-30 13:02:47 +0200

    update to exchoice. added flSettings and implemented it.

--- app/src/main/java/mad/saffi/ExChoiceActivity.kt
+++ app/src/main/java/mad/saffi/ExChoiceActivity.kt
@@ -1,11 +1,65 @@
 package mad.saffi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.Slide
+import android.transition.TransitionManager
+import android.view.Gravity
+import android.widget.RelativeLayout
+import androidx.core.view.isVisible
+import mad.saffi.databinding.ActivityExChoiceBinding
+import mad.saffi.databinding.ActivityExhibitionBinding
 
 class ExChoiceActivity : AppCompatActivity() {
+    //binding
+    lateinit var ExChoiceBinding: ActivityExChoiceBinding
+    lateinit var ExChoiceView: RelativeLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ex_choice)
+        ExChoiceBinding = ActivityExChoiceBinding.inflate(layoutInflater)
+        ExChoiceView = ExChoiceBinding.root
+        setContentView(ExChoiceView)
+
+        //----------------------------------------------------------------------------------------//
+        //Settings icon click
+        ExChoiceBinding.mtExChoice.setOnMenuItemClickListener { menuItem ->
+            if (menuItem.itemId == R.id.account) {
+                val rightSlide = Slide()
+                rightSlide.slideEdge = Gravity.END
+                TransitionManager.beginDelayedTransition(ExChoiceBinding.flSettings, rightSlide)
+                ExChoiceBinding.flSettings.isVisible =
+                    !ExChoiceBinding.flSettings.isVisible
+                true
+            } else {
+                false
+            }
+        }
+        //----------------------------------------------------------------------------------------//
+        //Back click
+        ExChoiceBinding.mtExChoice.setOnClickListener()
+        {
+            val intent = Intent(this, ExhibitionActivity::class.java)
+            startActivity(intent)
+        }
+        //----------------------------------------------------------------------------------------//
+        //On background click
+        //Closes all fragments
+        ExChoiceBinding.rlExChoice.setOnClickListener()
+        {
+            if (ExChoiceBinding.flSettings.isVisible == true)
+            {
+                CloseSettings()
+            }
+        }
+    }
+    //----------------------------------------------------------------------------------------//
+    //Closes the settings fragment
+    private fun CloseSettings()
+    {
+        val slide = Slide()
+        slide.slideEdge = Gravity.END
+        TransitionManager.beginDelayedTransition(ExChoiceBinding.flSettings, slide)
+        ExChoiceBinding.flSettings.isVisible = false
     }
 }
