240bb6ecf7332df6278d8f81466a155044a4974f
MAPrinsloo <notmattherwprinsloo@gmail.com>
2023-10-31 13:06:56 +0200

    updated Social tab

--- app/src/main/java/mad/saffi/SocialFragment.kt
+++ app/src/main/java/mad/saffi/SocialFragment.kt
@@ -1,59 +1,54 @@
 package mad.saffi
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import mad.saffi.databinding.FragmentSettingsBinding
+import mad.saffi.databinding.FragmentSocialBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SocialFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SocialFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    lateinit var SocialBinding: FragmentSocialBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_social, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SocialFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SocialFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        SocialBinding = FragmentSocialBinding.inflate(inflater, container, false)
+
+        SocialBinding.ivFacebook.setOnClickListener {
+            val facebookUrl = getString(R.string.facebook_link)
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(facebookUrl))
+            startActivity(intent)
+        }
+
+        SocialBinding.ivWeb.setOnClickListener {
+            val webUrl = getString(R.string.web_link)
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(webUrl))
+            startActivity(intent)
+        }
+
+        SocialBinding.ivEmail.setOnClickListener {
+            val email = getString(R.string.email_link)
+            val intent = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:$email"))
+            startActivity(intent)
+        }
+
+        SocialBinding.ivPhone.setOnClickListener {
+            val phoneNumber = getString(R.string.phone_link)
+            val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+            startActivity(intent)
+        }
+
+
+
+        return SocialBinding.root
+
     }
 }
